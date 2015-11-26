8e771529cbcc0aeb98ef076f6d693d89ea024640
wangyh9 <wangyh9@asiainfo.com>
2015-11-26 16:15:59 +0800

    create project

---
+++ WeChat/WeChat-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  WeChat-Bridging-Header.h
+//  WeChat
+//
+//  Created by yh_SunShine on 15/11/26.
+//  Copyright © 2015年 yhsunshine. All rights reserved.
+//
+
+#ifndef WeChat_Bridging_Header_h
+#define WeChat_Bridging_Header_h
+
+
+#endif /* WeChat_Bridging_Header_h */
+
+
+
+
