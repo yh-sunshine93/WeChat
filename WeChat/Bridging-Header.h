993f5c22a8bcd1d324532e6814fc972f44126aba
wangyh9 <wangyh9@asiainfo.com>
2015-11-26 16:00:26 +0800

    create project

---
+++ WeChat/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-header.h
+//  WeChat
+//
+//  Created by yh_SunShine on 15/11/26.
+//  Copyright © 2015年 yhsunshine. All rights reserved.
+//
+
+#ifndef Bridging_header_h
+#define Bridging_header_h
+#import "RootViewController.h"
+
+#endif /* Bridging_header_h */
